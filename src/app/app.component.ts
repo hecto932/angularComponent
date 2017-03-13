eb3c5e1e044bfb169202358b35a5ada843d9024d
Hector Flores <hecto932@gmail.com>
2017-03-12 18:35:02 -0600

    My first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hola';
+  nombre = 'Javascript'
+
+  eventClick():void{
+    this.nombre = this.nombre.toUpperCase()
+    console.log('Hola estoy escuchando')
+  }
 }
